2b44d1e773e746c9052efd12b017542c1c62a84b
Błażej <131681381+Warrgun@users.noreply.github.com>
2024-07-04 21:14:29 +0200

    bootstrap

--- markdown-previewer-app/src/App.jsx
+++ markdown-previewer-app/src/App.jsx
@@ -3,20 +3,21 @@ import {Link, Route, Routes} from 'react-router-dom'
 import Home from './Home'
 import NotFound from './NotFound'
 import './App.css'
+import { Button } from 'bootstrap'
 
 function App() {
-
   return (
     <>
-      <div className="navbar">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-          </ul>
+
+        <nav className='navbar navbar-expand bg-dark'>
+          <div className="container">
+            <ul className='navbar-nav'>
+                <li className='nav-item'>
+                  <Link to="/" className='nav-link'>Home</Link>
+                </li>
+              </ul>
+          </div>
         </nav>
-      </div>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='*' element={<NotFound/>}/>
