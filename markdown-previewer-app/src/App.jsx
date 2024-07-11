d4c031790b98b56b4041d3e77b4f9acf3bd46b86
Błażej <131681381+Warrgun@users.noreply.github.com>
2024-07-11 15:58:57 +0200

    navbar

--- markdown-previewer-app/src/App.jsx
+++ markdown-previewer-app/src/App.jsx
@@ -2,34 +2,42 @@ import { useState } from 'react'
 import {Link, Route, Routes} from 'react-router-dom'
 import Home from './Home'
 import NotFound from './NotFound'
-import {Navbar, Button, NavbarCollapse, Container, Nav} from 'react-bootstrap'
+import {Navbar, Button, Container, Nav, Offcanvas} from 'react-bootstrap'
 import './App.css'
 
 function App() {
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  
   return (
-    <>
-        <Navbar bg='dark' expand='lg' className='navbar-dark'>
+    <>  
+        <Navbar expand='lg' className='navbar-dark navbar-color border-bottom-color'>
           <Container fluid>
             <Navbar.Brand as={Link} to='/'>
               <img alt='Logo'/>
             </Navbar.Brand>
-              <Navbar.Toggle  data-bs-toggle='collapse' data-bs-target="#navbarNav" aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+              <Navbar.Toggle  onClick={handleShow} aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
                 <span className='navbar-toggler-icon'></span>
               </Navbar.Toggle>
-              <NavbarCollapse id='navbarNav'>
-                <div className="ms-auto d-flex align-items-center">\
-                  <Nav className='gap-5'>
-                    <Nav.Link as={Link} to='/' active aria-current='page'>Home</Nav.Link>
-                    <Nav.Link as={Link} to='/about-us' >About Us</Nav.Link>
-                    <Nav.Link href='Contact'>Contact</Nav.Link>
-                    <Nav.Link as={Link} to='/app'>START</Nav.Link>
-                  </Nav>
-                  <div className=" d-flex ms-3">
-                    <Button variant='outline-primary' type='button' className='btn-light rounded-0 m-2'>Learn</Button>
-                    <Button variant='outline-light' className='btn-dark rounded-0 m-2'>Sign Up</Button> 
-                  </div>
-                </div>
-                </NavbarCollapse>
+              <Navbar.Offcanvas show={show} onHide={handleClose} id='navbarNav' placement='end' className='text-bg-dark'>
+                <Offcanvas.Header className='border-bottom-color' closeButton>
+                  <Offcanvas.Title ><img alt='Logo'/></Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body className='d-flex flex-column flex-lg-row align-items-center p-4 p-lg-0'>
+                    <Nav className='justify-content-center justify-content-lg-end align-items-center flex-grow-1 gap-4'>
+                      <Nav.Link as={Link} to='/' active aria-current='page' onClick={handleClose}>Home</Nav.Link>
+                      <Nav.Link as={Link} to='/about-us' onClick={handleClose}>About Us</Nav.Link>
+                      <Nav.Link href='Contact' onClick={handleClose}>Contact</Nav.Link>
+                      <Nav.Link as={Link} to='/app' onClick={handleClose}>START</Nav.Link>
+                    </Nav>
+                    <div className=" d-lg-flex ms-lg-3">
+                      <Button variant='outline-primary' type='button' className='btn-light rounded-0 m-lg-2 me-2'>Learn</Button>
+                      <Button variant='outline-light' type='button' className='btn-dark rounded-0 m-lg-2'>Sign Up</Button> 
+                    </div>
+                </Offcanvas.Body>
+                </Navbar.Offcanvas>
               </Container>
           </Navbar>
       <Routes>
