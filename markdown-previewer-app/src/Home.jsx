da21799ed249da9a3fdd04b37dd7e612fbe4bd3d
Błażej <wargun364@gmail.com>
2024-11-14 02:04:51 +0100

    cards

--- markdown-previewer-app/src/Home.jsx
+++ markdown-previewer-app/src/Home.jsx
@@ -22,7 +22,7 @@ const Home = () =>{
                             </div>
                         </div>
                     </div>
-                    <div className="d-flex w-100">
+                    <div className="d-flex w-100 mb-5">
                         <div className="background-img me-5" style={{backgroundImage:`url(${pic1})`}}></div>
                         <div className="p-2 w-100">
                             <p>Simplistic</p>
@@ -46,6 +46,51 @@ const Home = () =>{
                             <div className="my-5">
                             <Button type="button" className="button-one rounded-0 fw-bold">Learn More</Button>
                             <Button type="button" className="button-three bg-transparent border-0 rounded-0 fw-bold ms-4">Sign Up<i className="arrow ms-2"></i></Button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="d-flex w-100">
+                        <div className="p-2 w-100 my-5">
+                            <p>Simplified</p>
+                            <h2>Expirience the Power of</h2>
+                            <h2>Mardown Previewer</h2>
+                        </div>
+                        <div className="p-2 w-100 my-5">
+                            <p className="fs-5 p-0">Our markdown previewer offers a range of key features to enhance your
+writing experience. With live preview, syntax highlighting, and export
+options, you can create beautiful and professional-looking documents
+effortlessly.</p>
+                        </div>  
+                    </div>
+                    <div className="d-flex flex-column w-100 mb-5">
+                    <div class="row text-center">
+                            <div class="col-4">
+                                <div class="card ">
+                                <img src="..." class="card-img-top" alt="..."/>
+                                    <div class="card-body">
+                                        <h3 class="card-title">Live Preview</h3>
+                                        <p class="card-text">See your changes in real-time as you type.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="card">
+                                <img src="..." class="card-img-top" alt="..."/>
+                                    <div class="card-body">
+                                        <h3 class="card-title">Syntax Highlighting</h3>
+                                        <p class="card-text">Highlight and differentiate your code for 
+                                        better readability.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="card">
+                                <img src="..." class="card-img-top" alt="..."/>
+                                    <div class="card-body">
+                                        <h3 class="card-title">Export Options</h3>
+                                        <p class="card-text">Save or share your documents in various formats.</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
