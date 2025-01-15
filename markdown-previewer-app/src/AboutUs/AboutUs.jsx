fd6157e2bbb1705d45b9fd80e38db4bc0a283e6c
Błażej <wargun364@gmail.com>
2025-01-15 19:20:52 +0100

    hfg

--- markdown-previewer-app/src/AboutUs/AboutUs.jsx
+++ markdown-previewer-app/src/AboutUs/AboutUs.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Card } from "react-bootstrap";
 import './aboutUs-custom.css'
@@ -5,45 +6,41 @@ import './aboutUs-custom.css'
 
 const AboutUs = () =>{
     return(
-        <Container fluid className="flex-grow-1 w-100 pt-5 about-container">
-            <div className="w-100 h-100 position-relative about-flex">
-                <div className="about-decorations circle position-absolute"></div>
-                <div className="about-decorations striped-circle  position-absolute"></div>
-                <div className="about-decorations profile-circle  position-absolute">
-                    <img
-                    src="https://via.placeholder.com/150"
-                    alt="Profile"
-                    className="profile-image"
-                    />
-                </div>
-                <div className="about-rect position-absolute"></div>
-                <div className="about-rect about-rect-shadow position-absolute"></div>
-                <div className="about-rect-striped position-absolute"></div>
-                <Card className="about-card mx-auto border-0 p-2 p-sm-5 my-5 my-sm-0">
-                    <Card.Body>
-                        <Card.Title className="pt-md-2 about-title" >ABOUT</Card.Title>
-                        <Row>
-                            <Col xs={12} md={6}>
-                                <Card.Text>
-                                    At MarkdownWorks, we are passionate about building tools that empower creators, developers, and writers to bring their ideas to life.
-                                    Our company was founded on the belief that simplicity and efficiency should go hand-in-hand when it comes to digital tools.
-                                </Card.Text>
-                            </Col>
-                            <Col xs={12} md={6} className="card-divider mt-3 mt-md-0">
-                                <Card.Text>
-                                    <h5 className=" fw-bold">Our Mission</h5>
-                                    We create user-friendly and innovative tools that simplify complex tasks and empower individuals to express themselves with ease.
-                                    <br/><br/><h5 className=" fw-bold">Who We Are</h5>
-                                    Founded by creators for creators, MarkdownWorks bridges the gap in Markdown tools.
-                                    With a passion for open-source technology and efficient workflows, we build tools that inspire productivity
-                                </Card.Text>
-                            </Col>
-                        </Row>
-                    </Card.Body>
-                </Card>
-            </div>
-            
-        </Container>
+        <>
+            <Container fluid className="p-0 flex-grow-1 d-flex flex-column ">
+                <div className="d-block w-100" style={{height:"73px"}}></div>
+                <Container fluid className="flex-grow-1 w-100 align-content-center about-container">
+                    <Row className="py-sm-5 p-md-0 custom-background">
+                        <Col xl={{span:6, offset:3}} lg={{span:8, offset:2}} sm={{span:10, offset:1}}>
+                        
+                            <Card className="about-card border-0 p-2 p-sm-5 my-5 my-sm-0">
+                                <Card.Body>
+                                    <Card.Title className="pt-md-2 about-title" >ABOUT</Card.Title>
+                                    <Row>
+                                        <Col xs={12} md={6}>
+                                            <Card.Text>
+                                                At MarkdownWorks, we are passionate about building tools that empower creators, developers, and writers to bring their ideas to life.
+                                                Our company was founded on the belief that simplicity and efficiency should go hand-in-hand when it comes to digital tools.
+                                            </Card.Text>
+                                        </Col>
+                                        <Col xs={12} md={6} className="card-divider mt-3 mt-md-0">
+                                            <Card.Text>
+                                                <span className=" fs-5 fw-bold">Our Mission</span><br/>
+                                                We create user-friendly and innovative tools that simplify complex tasks and empower individuals to express themselves with ease.
+                                                <br/><br/><span className=" fs-5 fw-bold">Who We Are</span><br/>
+                                                Founded by creators for creators, MarkdownWorks bridges the gap in Markdown tools.
+                                                With a passion for open-source technology and efficient workflows, we build tools that inspire productivity
+                                            </Card.Text>
+                                        </Col>
+                                    </Row>
+                                </Card.Body>
+                            </Card>  
+                        </Col>
+                    </Row>            
+                </Container>
+            </Container>
+        </>
+        
     );
 }
 
--- markdown-previewer-app/src/Contact/Contact.jsx
+++ markdown-previewer-app/src/Contact/Contact.jsx
@@ -34,31 +34,34 @@ const Contact = () =>{
     }  
 
     return(
-        <>
-            <Container fluid className={`flex-grow-1 d-flex d-sm-block align-content-center pt-sm-5 mb-0 pb-sm-4 ${styles.background}`}>
-                <Row className={`pt-5 ${styles.height}`}>
-                    <Col xl={{span:4, offset:4}} lg={{span:8, offset:2}} sm={{span:10, offset:1}} className="h-100 text-bg-dark px-5 pt-5 pb-4 py-sm-5 bg-opacity-75 align-content-center">
-                        <Form onSubmit={handleSubmit}>
-                            <div className="header">
-                                <h4>Let Us Help You</h4>
-                                <p>If you have any questions, feel free to ask!</p>
-                            </div>
-                            <Form.Group className="mb-3" controlId="name-input">
-                                <Form.Control type="text" name="name" placeholder="Name" className={`bg-transparent border-0 border-bottom rounded-0 ${styles.input} ${styles.boxShadow}`} value={content.name} onChange={handleChange} />
-                            </Form.Group>
-                            <Form.Group className="mb-5" controlId="email-input">
-                                <Form.Control type="email" placeholder="E-mail" className={` bg-transparent border-0 border-bottom rounded-0 ${styles.input} ${styles.boxShadow}`} name="email" value={content.email} onChange={handleChange} />
-                            </Form.Group>
-                            <Form.Group className="mb-2" controlId="title-input">
-                                <Form.Control type="text" placeholder="Title" className={` bg-transparent border-0 border-top rounded-0 ${styles.input} ${styles.title}`} name='title' value={content.title} onChange={handleChange}/>
-                            </Form.Group>
-                            <Form.Group className="mb-4" controlId="textarea-input">
-                                <Form.Control as="textarea" rows={3} name="emailContent" className={` bg-transparent border-0 border-bottom rounded-0 ${styles.input} ${styles.boxShadow}`}  placeholder="Message" value={content.emailContent} onChange={handleChange} />
-                            </Form.Group>
-                            <Button type="submit" variant="secondary" className=""> Submit</Button>
-                        </Form>
-                    </Col>
-                </Row>
+        <>  
+            <Container fluid className="p-0 flex-grow-1 d-flex flex-column ">
+                <div className="d-block w-100" style={{height:"73px"}}></div>
+                <Container fluid className={`flex-grow-1 d-flex d-sm-block align-content-center ${styles.background}`}>
+                    <Row className={`${styles.height}`}>
+                        <Col xl={{span:4, offset:4}} lg={{span:8, offset:2}} sm={{span:10, offset:1}} className="h-100 text-bg-dark px-5 py-4 py-sm-5 bg-opacity-75 align-content-center">
+                            <Form onSubmit={handleSubmit}>
+                                <div className="header">
+                                    <h4>Let Us Help You</h4>
+                                    <p>If you have any questions, feel free to ask!</p>
+                                </div>
+                                <Form.Group className="mb-3" controlId="name-input">
+                                    <Form.Control type="text" name="name" placeholder="Name" className={`bg-transparent border-0 border-bottom rounded-0 ${styles.input} ${styles.boxShadow}`} value={content.name} onChange={handleChange} />
+                                </Form.Group>
+                                <Form.Group className="mb-5" controlId="email-input">
+                                    <Form.Control type="email" placeholder="E-mail" className={` bg-transparent border-0 border-bottom rounded-0 ${styles.input} ${styles.boxShadow}`} name="email" value={content.email} onChange={handleChange} />
+                                </Form.Group>
+                                <Form.Group className="mb-2" controlId="title-input">
+                                    <Form.Control type="text" placeholder="Title" className={` bg-transparent border-0 border-top rounded-0 ${styles.input} ${styles.title}`} name='title' value={content.title} onChange={handleChange}/>
+                                </Form.Group>
+                                <Form.Group className="mb-4" controlId="textarea-input">
+                                    <Form.Control as="textarea" rows={3} name="emailContent" className={` bg-transparent border-0 border-bottom rounded-0 ${styles.input} ${styles.boxShadow}`}  placeholder="Message" value={content.emailContent} onChange={handleChange} />
+                                </Form.Group>
+                                <Button type="submit" variant="secondary" className=""> Submit</Button>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
             </Container>
         </>
     );
