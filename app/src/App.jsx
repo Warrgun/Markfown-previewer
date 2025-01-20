f40ddab91deb972f269c6432a899bd1cd40af8ec
Błażej <wargun364@gmail.com>
2025-01-20 14:43:30 +0100

    picture to public

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
-import {Button, Col, Container, FormControl, Row} from 'react-bootstrap';
+import { Button, Col, Container, FormControl, Row } from 'react-bootstrap';
 import Markdown from './Markdown';
 import NavbarComponent from './NavbarComponent';
 import './App.css'
 import ExportButton from './ExportButton';
 
-function App(){
+function App() {
   const [editor, setEditor] = useState(
-`# Markdown Previewer
+    `# Markdown Previewer
 ## Make websites easier then ever!
 Go back to the [Landing page](https://www.youtube.com)
 ###### Inline code:
@@ -22,22 +22,22 @@ Go back to the [Landing page](https://www.youtube.com)
 ###### Blockquote:
 > One day or day one?
 
-![stork](./src/assets/picture.png)`
-    );
+![stork](/assets/picture.png)`
+  );
 
-  const handleTabPress = (e) =>{
-    if(e.key === 'Tab'){
+  const handleTabPress = (e) => {
+    if (e.key === 'Tab') {
       e.preventDefault();
 
-      const currentPosition= e.target.selectionStart;
-      const textBefore  = editor.substring(0,currentPosition);
+      const currentPosition = e.target.selectionStart;
+      const textBefore = editor.substring(0, currentPosition);
       const textAfter = editor.substring(currentPosition);
 
-      setEditor(textBefore+'\t'+textAfter);
+      setEditor(textBefore + '\t' + textAfter);
 
-      setTimeout(()=>{
-        e.target.selectionStart = e.target.selectionEnd = currentPosition+1;
-      },0)
+      setTimeout(() => {
+        e.target.selectionStart = e.target.selectionEnd = currentPosition + 1;
+      }, 0)
     }
   }
 
@@ -48,16 +48,16 @@ Go back to the [Landing page](https://www.youtube.com)
   return (
     <>
       <Container fluid className='d-flex flex-column justify-content-lg-center w-100 h-100' >
-      <NavbarComponent exportFile={editor}/>
+        <NavbarComponent exportFile={editor} />
         <Row className=' markdown-row flex-grow-1 flex-lg-grow-0 mb-2 mx-2 mb-md-4 mx-lg-0 mb-lg-0'>
           <Col xs={12} md={6} lg={{ span: 5, offset: 1 }} xl={{ span: 4, offset: 2 }} className='h-100 text-area pe-2 pe-lg-3 ps-0 '>
-            <FormControl as='textarea' id='editor' value={editor} onChange={handleChange} onKeyDown={handleTabPress} className='h-100 scrollbar text-light fw-bold border-1 border-light'/>
+            <FormControl as='textarea' id='editor' value={editor} onChange={handleChange} onKeyDown={handleTabPress} className='h-100 scrollbar text-light fw-bold border-1 border-light' />
           </Col>
           <Col xs={12} md={6} lg={{ span: 5, offset: 0 }} xl={{ span: 4, offset: 0 }} className=' bg-light p-2 h-100 markdown-prev rounded'>
-            <Markdown markdown={editor}/>
+            <Markdown markdown={editor} />
           </Col>
           <Col lg={{ span: 5, offset: 6 }} xl={{ span: 4, offset: 6 }} className=' d-none d-lg-flex justify-content-end pt-2 px-0'>
-            <ExportButton exportFile={editor}/>
+            <ExportButton exportFile={editor} />
           </Col>
         </Row>
       </Container>
